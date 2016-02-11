ca7eeb419c2f88bd27406178a8cc1d1d5c9e0cb1
Jesper Westell <jesperwestell@hotmail.com>
2016-02-11 16:19:15 +0100

    LAB 2 finished

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -143,5 +143,6 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+bool thread_less_func(struct list_elem *a, struct list_elem *b, void *aux);
 
 #endif /* threads/thread.h */
