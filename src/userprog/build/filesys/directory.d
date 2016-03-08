a4bf4c68a3877fe61c501ff67bbe68b69b863be5
Richard Wigren <ricwi102@li15-12.ida.liu.se>
2016-03-08 12:05:46 +0100

    maybe done?? yaaay??

--- src/userprog/build/filesys/directory.d
+++ src/userprog/build/filesys/directory.d
@@ -3,4 +3,5 @@ filesys/directory.o: ../../filesys/directory.c ../../filesys/directory.h \
  ../../lib/inttypes.h ../../lib/stdint.h ../../lib/stdio.h \
  ../../lib/debug.h ../../lib/stdarg.h ../../lib/kernel/stdio.h \
  ../../lib/string.h ../../lib/kernel/list.h ../../filesys/filesys.h \
- ../../filesys/off_t.h ../../filesys/inode.h ../../threads/malloc.h
+ ../../filesys/off_t.h ../../threads/synch.h ../../filesys/inode.h \
+ ../../threads/malloc.h
--- src/userprog/build/filesys/fsutil.d
+++ src/userprog/build/filesys/fsutil.d
@@ -4,5 +4,5 @@ filesys/fsutil.o: ../../filesys/fsutil.c ../../filesys/fsutil.h \
  ../../lib/kernel/stdio.h ../../lib/stdlib.h ../../lib/string.h \
  ../../filesys/directory.h ../../devices/disk.h ../../lib/inttypes.h \
  ../../filesys/file.h ../../filesys/off_t.h ../../filesys/filesys.h \
- ../../threads/malloc.h ../../threads/palloc.h ../../threads/vaddr.h \
- ../../threads/loader.h
+ ../../threads/synch.h ../../lib/kernel/list.h ../../threads/malloc.h \
+ ../../threads/palloc.h ../../threads/vaddr.h ../../threads/loader.h
